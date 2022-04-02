aa1dd4f7d9bfe3bfa8f51dc551d34d31e12d85ea
GalSanyi <galsanyi84@gmail.com>
2022-04-02 12:14:41 +0200

    new

--- src/components/App.js
+++ src/components/App.js
@@ -6,70 +6,128 @@ import Modal from './Modal/Modal';
 import '../components/App.css';
 import Loader from './Loader/Loader';
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    isPending: false,
-    isModalOpen: false,
-    images: [],
-    modalImg: '',
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isPending, setIsPending] = useState(false);
+  const [isModalOpen, setIsMOdalOpen] = useState(false);
+  const [image, setImage] = useState([]);
+  const [modalImg, setModalImg] = useState('');
+
+  const handleSetQuery = ({ target: { name, value } }) => {
+    setQuery({ [name]: value });
   };
 
-  handleSetQuery = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  handleSubmitForm = event => {
+  const handleSubmitForm = event => {
     event.preventDefault();
-    this.setState({ isPending: true, page: 1 });
+    setIsPending({ isPending: true, page: 1 });
   };
-  componentDidUpdate() {
-    if (this.state.isPending) {
-      fetchImages(this.state.query, this.state.page).then(img => {
+
+  useEffect(() => {
+    if (isPending) {
+      fetchImages(query, page).then(img => {
         this.setState(prev => ({
-          images: this.state.page > 1 ? [...prev.images, ...img] : img,
+          images: page > 1 ? [...prev.images, ...img] : img,
           isPending: false,
         }));
       });
     }
-  }
-  handleToggleModal = image => {
+  });
+
+  const handleToggleModal = image => {
     this.setState(prev => ({
       isModalOpen: !prev.isModalOpen,
       modalImg: image,
     }));
   };
-  handleLoadMore() {
-    this.setState(prev => ({ page: prev.page + 1, isPending: true }));
-  }
-  render() {
-    const { query, images, isModalOpen, modalImg, isPending } = this.state;
-    const {
-      handleSetQuery,
-      handleSubmitForm,
-      handleToggleModal,
-      handleLoadMore,
-    } = this;
-    return (
-      <div>
-        <Searchbar
-          handleSetQuery={handleSetQuery}
-          query={query}
-          handleSubmitForm={handleSubmitForm}
-        />
-        <ImageGallery handleToggleModal={handleToggleModal} images={images} />
-        {images.length >= 12 && (
-          <Button handleLoadMore={handleLoadMore.bind(this)} />
-        )}
+  const handleLoadMore = () => {
+    setPage(prev => ({ page: prev.page + 1, isPending: true }));
+  };
 
-        {isPending && <Loader />}
-        {isModalOpen && (
-          <Modal modalImg={modalImg} handleToggleModal={handleToggleModal} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Searchbar
+        handleSetQuery={handleSetQuery}
+        query={query}
+        handleSubmitForm={handleSubmitForm}
+      />
+      <ImageGallery handleToggleModal={handleToggleModal} images={image} />
+      {image.length >= 12 && (
+        <Button handleLoadMore={handleLoadMore.bind(this)} />
+      )}
+
+      {isPending && <Loader />}
+      {isModalOpen && (
+        <Modal modalImg={modalImg} handleToggleModal={handleToggleModal} />
+      )}
+    </div>
+  );
 }
+
+// export default class App extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     isPending: false,
+//     isModalOpen: false,
+//     images: [],
+//     modalImg: '',
+//   };
+
+//   handleSetQuery = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmitForm = event => {
+//     event.preventDefault();
+//     this.setState({ isPending: true, page: 1 });
+//   };
+//   componentDidUpdate() {
+//     if (this.state.isPending) {
+//       fetchImages(this.state.query, this.state.page).then(img => {
+//         this.setState(prev => ({
+//           images: this.state.page > 1 ? [...prev.images, ...img] : img,
+//           isPending: false,
+//         }));
+//       });
+//     }
+//   }
+//   handleToggleModal = image => {
+//     this.setState(prev => ({
+//       isModalOpen: !prev.isModalOpen,
+//       modalImg: image,
+//     }));
+//   };
+//   handleLoadMore() {
+//     this.setState(prev => ({ page: prev.page + 1, isPending: true }));
+//   }
+//   render() {
+//     const { query, images, isModalOpen, modalImg, isPending } = this.state;
+//     const {
+//       handleSetQuery,
+//       handleSubmitForm,
+//       handleToggleModal,
+//       handleLoadMore,
+//     } = this;
+//     return (
+//       <div>
+//         <Searchbar
+//           handleSetQuery={handleSetQuery}
+//           query={query}
+//           handleSubmitForm={handleSubmitForm}
+//         />
+//         <ImageGallery handleToggleModal={handleToggleModal} images={images} />
+//         {images.length >= 12 && (
+//           <Button handleLoadMore={handleLoadMore.bind(this)} />
+//         )}
+
+//         {isPending && <Loader />}
+//         {isModalOpen && (
+//           <Modal modalImg={modalImg} handleToggleModal={handleToggleModal} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,35 +1,68 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import s from './Modal.module.css';
 
-export default class Modal extends Component {
-  onCloseModalByEsc = event => {
+export default function Modal({ handleToggleModal, modalImg }) {
+  const onCloseModalByEsc = event => {
     if (event.keyCode === 27) {
-      this.props.handleToggleModal('');
+      handleToggleModal('');
     }
   };
-  componentDidMount() {
-    window.addEventListener('keydown', this.onCloseModalByEsc);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onCloseModalByEsc);
-  }
-  render() {
-    const { handleToggleModal, modalImg } = this.props;
-    return (
-      <div>
-        <div
-          className={s.Overlay}
-          onClick={event => {
-            if (event.target === event.currentTarget) {
-              handleToggleModal('');
-            }
-          }}
-        >
-          <div className={s.Modal}>
-            <img src={modalImg} alt="" />
-          </div>
+
+  useEffect(() => {
+    window.addEventListener('keydown', onCloseModalByEsc);
+  });
+  useEffect(() => {
+    window.removeEventListener('keydown', onCloseModalByEsc);
+  });
+
+  return (
+    <div>
+      <div
+        className={s.Overlay}
+        onClick={event => {
+          if (event.target === event.currentTarget) {
+            handleToggleModal('');
+          }
+        }}
+      >
+        <div className={s.Modal}>
+          <img src={modalImg} alt="" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// export default class Modal extends Component {
+//   onCloseModalByEsc = event => {
+//     if (event.keyCode === 27) {
+//       this.props.handleToggleModal('');
+//     }
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onCloseModalByEsc);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onCloseModalByEsc);
+//   }
+//   render() {
+//     const { handleToggleModal, modalImg } = this.props;
+//     return (
+//       <div>
+//         <div
+//           className={s.Overlay}
+//           onClick={event => {
+//             if (event.target === event.currentTarget) {
+//               handleToggleModal('');
+//             }
+//           }}
+//         >
+//           <div className={s.Modal}>
+//             <img src={modalImg} alt="" />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
